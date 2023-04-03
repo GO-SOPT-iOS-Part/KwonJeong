d6fc86210e11214c6abcf0679ea897612ae915fb
kwonjeong <kwonjeong9470@gmail.com>
2023-04-03 21:59:06 +0900

    [Setting] #1 - 라이브러리 설치

---
+++ GO-SOPT-iOS/GO-SOPT-iOS/Presentation/1st_Seminar/FirstViewController.swift
@@ -0,0 +1,32 @@
+//
+//  FirstViewController.swift
+//  GO-SOPT-iOS
+//
+//  Created by KJ on 2023/04/03.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
