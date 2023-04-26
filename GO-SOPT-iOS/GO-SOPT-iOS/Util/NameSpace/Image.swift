dda37e8bd58c842aa2cd40fc720ceab2d075f01c
kwonjeong <kwonjeong9470@gmail.com>
2023-04-26 21:16:41 +0900

    [Setting] #6 - 에셋 추가

--- GO-SOPT-iOS/GO-SOPT-iOS/Util/NameSpace/Image.swift
+++ GO-SOPT-iOS/GO-SOPT-iOS/Util/NameSpace/Image.swift
@@ -14,5 +14,26 @@ public enum Image {
     public static let backButtonIcon = UIImage(named: "btn_before")!
     public static let passwordIcon = UIImage(named: "eye-slash")!
     public static let deleteIcon = UIImage(named: "x-circle")!
-    public static let tvingLogo = UIImage(named: "welcome")!
+    public static let welcomeImage = UIImage(named: "welcome")!
+    
+    // MARK: - Main
+    
+    public static let movieImg1 = UIImage(named: "movieImage1")!
+    public static let movieImg2 = UIImage(named: "movieImage2")!
+    public static let movieImg3 = UIImage(named: "movieImage3")!
+    public static let movieImg4 = UIImage(named: "movieImage4")!
+    public static let movieImg5 = UIImage(named: "movieImage5")!
+    public static let profileButton = UIImage(named: "profileButton")!
+    public static let tvingLogo = UIImage(named: "tvingLogo")!
+    
+    // MARK: - MyPage
+    
+    public static let jtbcLogo = UIImage(named: "jtbcLogo")!
+    public static let RightArrow = UIImage(named: "rightArrow")!
+    public static let profileImage = UIImage(named: "profile")!
+    public static let tvNLogo = UIImage(named: "tvNLogo")!
+    public static let myTicketImg = UIImage(named: "myTicket")!
+    public static let notificationImg = UIImage(named: "notification")!
+    public static let settingImg = UIImage(named: "setting")!
+    public static let tvingCacheImg = UIImage(named: "tvingCache")!
 }
