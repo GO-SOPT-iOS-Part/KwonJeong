ac04a30f401a972e6705a84266d807fd1c2435cf
kwonjeong <kwonjeong9470@gmail.com>
2023-04-27 17:10:26 +0900

    [Feat] #6 - BaseViewController 추가

---
+++ GO-SOPT-iOS/GO-SOPT-iOS/Presentation/Common/ViewController/BaseViewController.swift
@@ -0,0 +1,66 @@
+//
+//  BaseViewController.swift
+//  GO-SOPT-iOS
+//
+//  Created by KJ on 2023/04/27.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+class BaseViewController: UIViewController {
+    
+    // MARK: - UI Components
+    
+    // MARK: - Properties
+    
+    lazy private(set) var className: String = {
+      return type(of: self).description().components(separatedBy: ".").last ?? ""
+    }()
+    
+    // MARK: - Initializer
+    
+    override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    deinit {
+        print("DEINIT: \(className)")
+    }
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setLayout()
+    }
+}
+
+extension ViewController {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI() {
+        view.backgroundColor = Color.tvingBlack
+        
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - @objc Methods
+}
---
+++ GO-SOPT-iOS/GO-SOPT-iOS/Presentation/MyProfile/ViewController/MyProfileViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MyProfileViewController.swift
+//  GO-SOPT-iOS
+//
+//  Created by KJ on 2023/04/27.
+//
+
+import UIKit
+
+import SnapKit
+import Then
+
+final class MyProfileViewController: BaseViewController {
+    
+    // MARK: - UI Components
+    
+    // MARK: - Properties
+    
+    // MARK: - Initializer
+    
+    // MARK: - View Life Cycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setUI()
+        setLayout()
+    }
+}
+
+extension MyProfileViewController {
+    
+    // MARK: - UI Components Property
+    
+    private func setUI() {
+        
+    }
+    
+    // MARK: - Layout Helper
+    
+    private func setLayout() {
+        
+    }
+    
+    // MARK: - Methods
+    
+    // MARK: - @objc Methods
+}
